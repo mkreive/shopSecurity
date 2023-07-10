72022273986b7f5663f38d4814cd00138454385d
mkreive <mkreive@gmail.com>
2023-07-10 14:35:58 +0300

    security bcrypt added and roles and members added to db

--- src/main/java/lt/javau5/securityShop/security/SecurityConfiguration.java
+++ src/main/java/lt/javau5/securityShop/security/SecurityConfiguration.java
@@ -1,7 +1,45 @@
 package lt.javau5.securityShop.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    public UserDetailsManager userDetailsManager(DataSource dataSource) {
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+
+        jdbcUserDetailsManager.setUsersByUsernameQuery("select user_id, pw, active from members where user_id=?");
+
+        jdbcUserDetailsManager.setAuthoritiesByUsernameQuery("select user_id, role from roles where user_id=?");
+
+        return jdbcUserDetailsManager;
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configurer -> configurer
+                .requestMatchers(HttpMethod.GET, "/api/products").hasRole("CUSTOMER")
+                .requestMatchers(HttpMethod.GET, "/api/products/**").hasRole("CUSTOMER")
+                .requestMatchers(HttpMethod.POST, "/api/products").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/products").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/products/**").hasRole("ADMIN")
+        );
+
+        // use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        // disable Cross Site Request Forgery (CSRF) - for rest api
+        http.csrf(csrf -> csrf.disable());
+        return http.build();
+    }
 }
